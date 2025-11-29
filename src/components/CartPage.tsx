b5390457e895b5cd494179b3762be0a02b831508
aarushi787 <103561384+aarushi787@users.noreply.github.com>
2025-11-29 12:15:19 +0530

    Update CartPage.tsx

--- src/components/CartPage.tsx
+++ src/components/CartPage.tsx
@@ -174,6 +174,6 @@ const CartPage: React.FC<CartPageProps> = ({ cartItems, onUpdateQuantity, onRemo
     </div>
   );
 };
-await loadRazorpay();
+// await loadRazorpay();
 export default CartPage;
 
